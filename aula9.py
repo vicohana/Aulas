384f774c6352bd4553fadba50b48a014c7becfe9
vicohana <156628809+vicohana@users.noreply.github.com>
2024-01-18 16:29:19 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,14 @@
+adicao = 10 + 10
+print('Adição', adicao)
+subtracao = 10 - 10
+print('Subtração', subtracao)
+multiplicacao = 10 * 10
+print('multiplicação', multiplicacao)
+divisao = 10 / 2.54  #float
+print('Divisão', divisao)
+divisao_inteira = 10 // 2.3
+print('Divisão inteira', divisao_inteira)
+exponenciacao = 2 ** 10
+print('exponenciação', exponenciacao)
+modulo = 55 % 4
+print('Módulo', modulo)
