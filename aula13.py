ea95ec88295f60160e56484cac1413032c2489a4
vicohana <156628809+vicohana@users.noreply.github.com>
2024-01-18 16:32:25 -0300

    Create aula13.py

---
+++ aula13.py
@@ -0,0 +1,12 @@
+nome = 'Vicente'
+altura = 1.68
+peso = 88.2
+imc = peso / (altura ** 2)
+
+linha_1 = f'{nome}, tem, {altura:.2f},de altura,'
+linha_2 = f'pesa,{peso}, quilos e seu IMC é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
