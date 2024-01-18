eb30dbfd492d8536447dec357d99a939c5c446ed
vicohana <156628809+vicohana@users.noreply.github.com>
2024-01-18 16:27:36 -0300

    aula6.py

---
+++ aula6.py
@@ -0,0 +1,15 @@
+# conversão de tipo, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool 
+print(1 + 1)
+#para str a soma o python concatena as str,
+# porém isso não acontece com str e outro tipo de variavel
+print("a" + "b") 
+print(int('1'), type(int('1')))
+print(int('1') + 1)
+#o python le de dentro para fora, do () de dentro para o mais exterior
+print(float('1') + 1)
+print(bool(''))
+print(str(11) + 'b')
